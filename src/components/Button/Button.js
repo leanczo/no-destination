1b19a818bbd5433d0165c2848f9a74182390f028
Leandro Cardozo <50833557+leanczo@users.noreply.github.com>
2023-08-06 12:42:37 -0300

    🔨 add font family and others changes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,11 @@
 import "./Button.css";
 
-const Button = ({onClick}) => {
-  return <div className="button-container">
-    <input className="button" type="button" value="Press me" onClick={onClick} />
-    </div>;
+const Button = ({ onClick }) => {
+  return (
+    <div className="button-container">
+      <input className="button" type="button" value="Clear" onClick={onClick} />
+    </div>
+  );
 };
 
 export default Button;
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,27 +1,41 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Input.css";
 
-const Input = ({  hasToDestroy, resetDestroy }) => {
-  const [text, setText] = useState("");
+const Input = ({ hasToDestroy, resetDestroy }) => {
+  const [letters, setLetters] = useState([""]);
+  const [text, setText] = useState();
 
   const handleChange = (e) => {
-    setText(e.target.value)
+    const value = e.target.value;
+    setLetters(value.split(""));
+    setText(value);
   };
 
   useEffect(() => {
     if (hasToDestroy) {
+      setText("");
       setTimeout(() => {
         resetDestroy();
-        setText("");
-      }, 5000);
+        setLetters([]);
+      }, 500 * letters.length);
     }
-  }, [hasToDestroy, resetDestroy]);
+  }, [hasToDestroy, resetDestroy, letters.length]);
 
   return (
     <div className="input-container">
-      <textarea
+      <div className={`letters-container ${hasToDestroy ? "animate" : ""}`}>
+        {letters.map((letter, index) => (
+          <span
+            className="letter"
+            style={{ animationDelay: `${index * 50}ms` }}
+          >
+            {letter}
+          </span>
+        ))}
+      </div>
+      <input
         placeholder="Write your frustrations here"
-        className={`input ${hasToDestroy ? 'destroy' : ''}`}
+        className="input"
         onChange={handleChange}
         value={text}
       />
